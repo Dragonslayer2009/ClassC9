0b98d155c0058045e225423206c1fa0ec7c0724d
Dragonslayer2009 <91575459+Dragonslayer2009@users.noreply.github.com>
2021-09-29 09:52:15 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,30,30);
+}
+
+function draw() 
+{
+  background(30);
+
+  if(keyDown("up")) {
+    box.position.y = box.position.y - 3;
+  }
+
+  if(keyDown("down")){
+    box.position.y = box.position.y + 3;
+  }
+
+  if(keyDown("right")){
+    box.position.x = box.position.x + 3;
+  }
+
+  if(keyDown("left")){
+    box.position.x = box.position.x - 3;
+  }
+
+  drawSprites();
+}
